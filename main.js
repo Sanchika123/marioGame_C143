9bb57d53e1b3541c26274dda8d2d454a128f75c9
Sanchika123 <79789552+Sanchika123@users.noreply.github.com>
2021-11-27 12:54:18 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,48 @@
+function preload() {
+	world_start = loadSound("world_start.wav");
+	mario_jump= loadSound('jump.wav',function(){
+		console.log('loaded','jump.wav');
+	});
+	mario_coin= loadSound('coin.wav');
+	mario_gameover= loadSound('gameover.wav');
+	mario_killing= loadSound('kick.wav');
+	mario_die= loadSound('mariodie.wav');
+	setSprites();
+	MarioAnimation();
+}
+
+function setup() {
+	canvas = createCanvas(1240,336);
+	canvas.parent('canvas');
+	video = createCapture(VIDEO);
+	video.size(800,400);
+	video.parent('game_console');
+	instializeInSetup(mario);
+
+poseNet = ml5.poseNet(video, modelLoaded);
+  poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded() {
+	console.log('Model Loaded!');
+  }
+  
+  function gotPoses(results)
+  {
+	if(results.length > 0)
+	{
+	  noseX = results[0].pose.nose.x;
+	  noseY = results[0].pose.nose.y;
+	}
+  }
+  
+
+function draw() {
+	game()
+}
+
+
+
+
+
+
